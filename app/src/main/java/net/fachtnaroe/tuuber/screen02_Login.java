0002e1793dfefa4ae8b4ebba055342a10ed6d681
Ryker-Smith <fachtnaroe@gmail.com>
2019-01-09 19:26:02 +0000

    Opens next Activity screen.

--- app/src/main/java/net/fachtnaroe/tuuber/screen02_Login.java
+++ app/src/main/java/net/fachtnaroe/tuuber/screen02_Login.java
@@ -1,6 +1,7 @@
 package net.fachtnaroe.tuuber;
 // http://thunkableblocks.blogspot.ie/2017/07/java-code-snippets-for-app-inventor.html
 
+import com.google.appinventor.components.runtime.ActivityStarter;
 import com.google.appinventor.components.runtime.Button;
 import com.google.appinventor.components.runtime.Component;
 import com.google.appinventor.components.runtime.EventDispatcher;
@@ -28,12 +29,12 @@ import org.json.JSONObject;
 
 public class screen02_Login extends Form implements HandlesEventDispatching {
 
-    private TextBox textMine, varName;
-    private Button buttonMine, LoginButton;
+//    private TextBox;
+    private Button LoginButton;
     private Image Header, Image2, Image3, Image4;
 
     private Web LoginWeb;
-    private VerticalScrollArrangement Login;
+    private VerticalArrangement Login;
     private Notifier Notifier1;
 
     private VerticalArrangement VerticalArrangement1;
@@ -48,57 +49,65 @@ public class screen02_Login extends Form implements HandlesEventDispatching {
 
     protected void $define() {
 
-        Login = new VerticalScrollArrangement(this);
+        Login = new VerticalArrangement(this);
         Login.WidthPercent(100);
         Login.HeightPercent(100);
-        Login.Image();
+        Login.Image("img_splashcanvas.png");
+        Notifier1 = new Notifier(Login);
+
         Header = new Image (Login);
-
-
+        Header.Picture("img_carlogo.png");
 
         VerticalArrangement1 = new VerticalArrangement (Login);
         HorizontalArrangement1 = new HorizontalArrangement(VerticalArrangement1);
         LoginWeb = new Web(Login);
-
         Image4 = new Image(VerticalArrangement1);
-        Image4.Picture("");
 
         HorizontalArrangement2 = new HorizontalArrangement(VerticalArrangement1);
         UserNameLabel= new Label (HorizontalArrangement2);
         UserNameLabel.Text("Username:");
-        UserNameLabel.WidthPercent(40);
+        UserNameLabel.FontBold(true);
+        //UserNameLabel.WidthPercent(40);
         UserName= new TextBox (HorizontalArrangement2);
-        UserName.WidthPercent(60);
+        UserName.BackgroundColor(Component.COLOR_WHITE);
+        UserName.Text("htna.roe@tcfe.ie");
 
         HorizontalArrangement4 = new HorizontalArrangement(VerticalArrangement1);
         PasswordLabel = new Label(HorizontalArrangement4);
-        PasswordLabel.WidthPercent(40);
+        //PasswordLabel.WidthPercent(40);
         PasswordLabel.Text("Password");
+        PasswordLabel.FontBold(true);
         Password = new PasswordTextBox(HorizontalArrangement4);
+        Password.Text("tcfetcfe");
 
         HorizontalArrangement3 = new HorizontalArrangement(VerticalArrangement1);
         Image2 = new Image(HorizontalArrangement4);
+        Image2.Picture("img_icon_fast.png");
         LoginButton = new Button (HorizontalArrangement3);
         LoginButton.Text("Login");
-
         Image3 = new Image(HorizontalArrangement4);
+//        Image3.Picture("img_carlogo.png");
 
         localDB = new TinyDB(this);
-
+        String prevName = (String) localDB.GetValue("Username","-1");
+        if ( !prevName.equals("-1")) {
+            UserName.Text(prevName);
+        }
+        localDB.StoreValue("TestTag", "67890");
         EventDispatcher.registerEventForDelegation(this, "LoginButton", "Click");
         EventDispatcher.registerEventForDelegation(this, "LoginWeb", "GotText");
     }
 
     public boolean dispatchEvent(Component component, String componentName, String eventName, Object[] params) {
-        if (component.equals(buttonMine) && eventName.equals("Click")) {
+        if (component.equals(LoginButton) && eventName.equals("Click")) {
 
             LoginWeb.Url(
                     baseURL +
-                    "name=" +
-                    varName.Text() +
-                    "&value=" +
-                    textMine.Text() +
-                    "&method=POST"
+                    "email=" +
+                    UserName.Text() +
+                    "&password=" +
+                    Password.Text() +
+                    "&cmd=LOGIN"
             );
             LoginWeb.Get();
             return true;
@@ -110,7 +119,7 @@ public class screen02_Login extends Form implements HandlesEventDispatching {
             String encoding = (String) params[2];
             String textOfResponse = (String) params[3];
 
-            webGotText(status.toString());
+            webGotText(status.toString(), textOfResponse);
             return true;
         }
         else {
@@ -120,18 +129,30 @@ public class screen02_Login extends Form implements HandlesEventDispatching {
         return false;
     }
 
-    public void webGotText(String status) {
-
-        try {
-            JSONObject parser = new JSONObject(status);
-            if (parser.getString("status").equals("OK"))
-            {
+    public void webGotText(String status, String textOfResponse) {
+        LoginButton.Text(status);
+        if (status.equals("200") ) try {
+            JSONObject parser = new JSONObject(textOfResponse);
+            if (parser.getString("result").equals("OK")) {
                 // do something
+
+                localDB.StoreValue("Username", 22);
+
+                ActivityStarter nextScreen = new ActivityStarter(this);
+                nextScreen.ActivityClass("net.fachtnaroe.tuuber.screen03_MainMenu");
+                nextScreen.ActivityPackage("net.fachtnaroe.tuuber");
+                nextScreen.StartActivity();
+
+            } else {
+                Notifier1.ShowMessageDialog("Login failed, check details", "Information", "OK");
             }
         } catch (JSONException e) {
             // if an exception occurs, code for it in here
+            Notifier1.ShowMessageDialog("JSON Exception", "Information", "OK");
+        }
+        else {
+            Notifier1.ShowMessageDialog("Problem connecting with server","Information", "OK");
         }
     }
-
 
 }
---
+++ app/src/main/java/net/fachtnaroe/tuuber/screen03_MainMenu.java
@@ -0,0 +1,29 @@
+package net.fachtnaroe.tuuber;
+
+import android.preference.PreferenceActivity;
+
+import com.google.appinventor.components.runtime.Form;
+import com.google.appinventor.components.runtime.HandlesEventDispatching;
+import com.google.appinventor.components.runtime.Image;
+import com.google.appinventor.components.runtime.Notifier;
+import com.google.appinventor.components.runtime.VerticalArrangement;
+
+public class screen03_MainMenu extends Form implements HandlesEventDispatching {
+
+    VerticalArrangement MainMenu;
+    Notifier Messages;
+    Image Header;
+
+    protected void $define() {
+
+        MainMenu = new VerticalArrangement(this);
+        MainMenu.WidthPercent(100);
+        MainMenu.HeightPercent(100);
+        MainMenu.Image("img_splashcanvas.png");
+        Messages = new Notifier(MainMenu);
+
+        Header = new Image(MainMenu);
+        Header.Picture("img_carlogo.png");
+    }
+
+    }
