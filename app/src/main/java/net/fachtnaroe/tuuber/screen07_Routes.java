b643dc46956b59650d1a46514d7fe2b020bbf817
Desmond.Delaney <desmond.delaney18@student.tcfe.ie>
2019-01-12 14:39:58 +0000

    2019-01-12.01 D.D Added more to layout of Screen_07Routes

--- app/src/main/java/net/fachtnaroe/tuuber/screen07_Routes.java
+++ app/src/main/java/net/fachtnaroe/tuuber/screen07_Routes.java
@@ -37,9 +37,9 @@ public class screen07_Routes extends Form implements HandlesEventDispatching {
     private Image TuberH, Image11, Image7, Image5, Image9, Image1, Image2;
     private Web NewRouteWeb, GetRouteWeb, Web3;
     private Notifier Notifier1, Notifier2;
-    private Label pID, InfoLabel, DriverLabel, Route1Label, OriginLabel, Destination, CheckDaysLabel, Origin2Label, Destination2Label, MonYN, Mon2YN, TueYN, Tue2YN, WedYN, Wed2YN, ThursYN, Thurs2YN, FriYN, Fri2YN;
+    private Label pID, InfoLabel, DriverLabel, Route1Label, OriginLabel, DestinationLabel, CheckDaysLabel, Origin2Label, Destination2Label, MonYN, Mon2YN, TueYN, Tue2YN, WedYN, Wed2YN, ThursYN, Thurs2YN, FriYN, Fri2YN;
     private HorizontalArrangement MainMenuHArrangement, HorizontalArrangement8, HorizontalArrangement11, HorizontalArrangement7, HorizontalArrangement12, HorizontalArrangement9, HorizontalArrangement13, HorizontalArrangement5, SaveRouteHArrangement;
-    private VerticalArrangement RoutesVArrangement, VerticalArrangement14, VerticalArrangement3, VerticalArrangement2, Routes;
+    private VerticalArrangement RoutesVArrangement, VerticalArrangement4, VerticalArrangement3, VerticalArrangement2, Routes;
     private Button MainMenu, SaveNew;
     private ListView TempList;
     private TextBox TownSingle, TownsDecoded, DriverYN;
@@ -53,6 +53,36 @@ public class screen07_Routes extends Form implements HandlesEventDispatching {
         MainMenuHArrangement = new HorizontalArrangement(Routes);
         pID = new Label(MainMenuHArrangement);
         MainMenu = new Button(MainMenuHArrangement);
+        MainMenu.Text("MAIN MENU");
+        HorizontalArrangement8 = new HorizontalArrangement(Routes);
+        TempList = new ListView(HorizontalArrangement8);
+        TownSingle = new TextBox(HorizontalArrangement8);
+        TownsDecoded = new TextBox(HorizontalArrangement8);
+        Image11 = new Image(Routes);
+        RoutesVArrangement = new VerticalArrangement(Routes);
+        VerticalArrangement4 = new VerticalArrangement(RoutesVArrangement);
+        HorizontalArrangement11 = new HorizontalArrangement(VerticalArrangement4);
+        InfoLabel = new Label(HorizontalArrangement11);
+        InfoLabel.Text("Please fill in route information below");
+        Image7 = new Image(HorizontalArrangement11);
+        DriverLabel = new Label(HorizontalArrangement11);
+        DriverLabel.Text("Driver Y / N");
+        DriverYN = new TextBox(HorizontalArrangement11);
+        DriverYN.Text("Y/N");
+        Image5 = new Image(HorizontalArrangement11);
+        VerticalArrangement3 = new VerticalArrangement(VerticalArrangement4);
+        Route1Label = new Label(VerticalArrangement3);
+        Route1Label.Text("ROUTE 1");
+        HorizontalArrangement7 = new HorizontalArrangement(VerticalArrangement3);
+        OriginLabel = new Label(HorizontalArrangement7);
+        OriginLabel.Text("ORIGIN");
+        OriginList = new ListPicker(HorizontalArrangement7);
+        OriginList.Text("Select Origin");
+        DestinationLabel = new Label(HorizontalArrangement7);
+        DestinationLabel.Text("Destination");
+        DestinationList = new ListPicker(HorizontalArrangement7);
+        DestinationList.Text("Templemore");
+        
 
     }
 }
