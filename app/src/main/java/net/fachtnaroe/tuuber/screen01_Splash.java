a357b36d410385f1b7006a72798e3bd3c88f87a8
Ryker-Smith <fachtnaroe@gmail.com>
2019-01-29 14:41:03 +0000

    2019-01-29.03: Some debug statements removed.

--- app/src/main/java/net/fachtnaroe/tuuber/screen01_Splash.java
+++ app/src/main/java/net/fachtnaroe/tuuber/screen01_Splash.java
@@ -9,6 +9,6 @@ import com.google.appinventor.components.runtime.HandlesEventDispatching;
 public class screen01_Splash extends Form implements HandlesEventDispatching {
 
     protected void $define(){
-
+        switchForm("screen02_Login");
     }
 }
