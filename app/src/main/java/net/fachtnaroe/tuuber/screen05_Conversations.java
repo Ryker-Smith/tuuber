ca31e8fbb1c8f9886c2e98c9e26bec6ac8bad6b9
Desmond.Delaney <desmond.delaney18@student.tcfe.ie>
2019-02-01 14:20:31 +0000

    2019.02.01.03

--- app/src/main/java/net/fachtnaroe/tuuber/screen05_Conversations.java
+++ app/src/main/java/net/fachtnaroe/tuuber/screen05_Conversations.java
@@ -1,42 +1,45 @@
 package net.fachtnaroe.tuuber;
 
 import com.google.appinventor.components.runtime.Button;
+import com.google.appinventor.components.runtime.Component;
+import com.google.appinventor.components.runtime.EventDispatcher;
 import com.google.appinventor.components.runtime.Form;
 import com.google.appinventor.components.runtime.HandlesEventDispatching;
 import com.google.appinventor.components.runtime.HorizontalArrangement;
 import com.google.appinventor.components.runtime.ListView;
 import com.google.appinventor.components.runtime.TextBox;
 import com.google.appinventor.components.runtime.VerticalArrangement;
+import com.google.appinventor.components.runtime.Web;
 
 public class screen05_Conversations extends Form implements HandlesEventDispatching {
 
     private tuuber_Settings applicationSettings;
-    private Button Send, OtherChats;
-    private HorizontalArrangement ChatHZ, SendHZ, OtherChatsHZ, ChatBoxHZ;
-    private VerticalArrangement Conversation;
-    private TextBox ChatBox;
+    private VerticalArrangement Conversations;
+    private HorizontalArrangement ContactsHZ, ChatsScreenHZ;
     private ListView Contacts;
-
-
+    private Button ChatsScreen;
 
     protected void $define() {
 
         applicationSettings = new tuuber_Settings(this);
         this.BackgroundImage(applicationSettings.backgroundImageName);
-        Conversation = new VerticalArrangement(this);
-        ChatHZ = new HorizontalArrangement(Conversation);
-        Contacts = new ListView(ChatHZ);
+        Conversations = new VerticalArrangement(this);
+        ContactsHZ = new HorizontalArrangement(Conversations);
+        Contacts = new ListView(ContactsHZ);
+        ChatsScreenHZ = new HorizontalArrangement(Conversations);
+        ChatsScreen = new Button(ChatsScreenHZ);
+        ChatsScreen.Text("Chat");
 
-        ChatBoxHZ = new HorizontalArrangement(Conversation);
-        ChatBox = new TextBox(ChatBoxHZ);
+        EventDispatcher.registerEventForDelegation(this, "ChatsScreen", "Click");
 
-        SendHZ = new HorizontalArrangement(Conversation);
-        Send = new Button(SendHZ);
-        Send.Text("Send");
 
-        OtherChatsHZ = new HorizontalArrangement(Conversation);
-        OtherChats = new Button(OtherChatsHZ);
-        OtherChats.Text("Other Chats");
+    }
 
+    public boolean dispatchEvent(Component component, String componentName, String eventName, Object[] params) {
+        if (component.equals(ChatsScreen) && eventName.equals("Click")) {
+            switchForm("screen08_ChatWith");
+            return true;
+        }
+        return true;
     }
 }
