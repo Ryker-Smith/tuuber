c1b9cff5d3082116a945588759d6e614bd959bab
Desmond.Delaney <desmond.delaney18@student.tcfe.ie>
2019-02-26 14:56:18 +0000

    2019.02.26.06

--- app/src/main/java/net/fachtnaroe/tuuber/screen05_Conversations.java
+++ app/src/main/java/net/fachtnaroe/tuuber/screen05_Conversations.java
@@ -194,7 +194,7 @@ public class screen05_Conversations extends Form implements HandlesEventDispatch
                     if (contacts1Array.getJSONObject(i).toString().equals("{}")) break;
                     ListofContactWeb1.add(
                             contacts1Array.getJSONObject(i).getString("initiator_pID" ) +
-                                    ": " +
+                                    ":: " +
                                     contacts1Array.getJSONObject(i).getString("first" ) +
                                     " " +
                                     contacts1Array.getJSONObject(i).getString("family")
