fcdc021e62960d6294e1ab0fbdaa1d2fce4e0aba
Alexsander <aleksadner.kubno18@student.tcfe.ie>
2019-03-05 12:11:34 +0000

    2019-05-03 Matches screen Progress

--- app/src/main/java/net/fachtnaroe/tuuber/screen04_Matches.java
+++ app/src/main/java/net/fachtnaroe/tuuber/screen04_Matches.java
@@ -25,10 +25,11 @@ import java.util.List;
 public class screen04_Matches extends Form implements HandlesEventDispatching {
 
     private tuuber_Settings applicationSettings;
-    private Button SelectMyRout, MainMenu, Send;
+    private Button SelectMyRout, MainMenu, DisplayDetails, Refresh;
     private VerticalArrangement Matches, VerticalArrangment1, VerticalArrangment2;
-    private HorizontalArrangement HorizontalArragment1, HorizontalArragment2, HorizontalArragment3;
+    private HorizontalArrangement MatchesButtons, MenuButtons, HorizontalArragment3;
     private ListView MyRouteList, MatchesMade;
+    private Label User_ID;
     private String baseURL = "https://fachtnaroe.net/tuuber";
 
 
@@ -40,11 +41,33 @@ public class screen04_Matches extends Form implements HandlesEventDispatching {
         Matches = new VerticalArrangement(this);
         Matches.HeightPercent(100);
         Matches.WidthPercent(100);
+        MenuButtons = new HorizontalArrangement(Matches);
+        MainMenu = new Button(MenuButtons);
+        MainMenu.Height(40);
+        MainMenu.Width(40);
+        MainMenu.Image("buttonHome.png");
+        User_ID = new Label(MenuButtons);
+        User_ID.Text("I am user: #" + applicationSettings.pID);
+        User_ID.FontSize(20);
+        User_ID.Height(40);
+        User_ID.WidthPercent(70);
+        User_ID.TextAlignment(Component.ALIGNMENT_CENTER);
+        Refresh = new Button(MenuButtons);
+        Refresh.Image("buttonRefresh.png");
         MyRouteList = new ListView(Matches);
+        MyRouteList.HeightPercent(35);
+        MatchesButtons = new HorizontalArrangement(Matches);
+        SelectMyRout =new Button(MatchesButtons);
+        SelectMyRout.Text("Send Messege");
+
         MatchesMade = new ListView(Matches);
+        MatchesMade.HeightPercent(35);
+        DisplayDetails= new Button(Matches);
+        DisplayDetails.Text("Display Details");
 
         EventDispatcher.registerEventForDelegation(this, "buttonMainMenu", "Click");
         EventDispatcher.registerEventForDelegation(this, "none", "BackPressed");
+        EventDispatcher.registerEventForDelegation(this, formName, "GotText");
     }
 
     public boolean dispatchEvent(Component component, String componentName, String eventName, Object[] params) {
