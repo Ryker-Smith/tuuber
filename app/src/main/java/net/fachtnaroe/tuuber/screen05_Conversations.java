a2a06f521524cce806684f8b8599a50746ef58e2
Desmond.Delaney <desmond.delaney18@student.tcfe.ie>
2019-05-23 13:09:55 +0100

    2019.05.23.03

--- app/src/main/java/net/fachtnaroe/tuuber/screen05_Conversations.java
+++ app/src/main/java/net/fachtnaroe/tuuber/screen05_Conversations.java
@@ -122,7 +122,7 @@ public class screen05_Conversations extends Form implements HandlesEventDispatch
         button_Pad_Separate_Accept_Decline.Width(5);
 
         button_DeclineInbound = new Button(InboundButtonsHZ);
-        button_DeclineInbound.Text("Decline inbound");
+        button_DeclineInbound.Text(tools.fn_téacs_aistriú("decline_inbound",tools.capitalize_none));
         tools.buttonOnOff(button_DeclineInbound,false);
 
         vt_Out = new VerticalArrangement(Conversations);
